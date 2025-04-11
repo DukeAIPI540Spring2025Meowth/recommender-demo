10ee8fd44dbe2d239e0b1b0ecb13533b23398a3b
haranku16 <haranku16@users.noreply.github.com>
2025-04-11 17:01:44 -0400

    Extracting dataset from Kaggle (#10)

--- scripts/etl/etl.py
+++ scripts/etl/etl.py
@@ -1,12 +1,23 @@
-def extract():
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def extract() -> tuple[pd.DataFrame, pd.DataFrame]:
     '''
     Extract the data from the source
     '''
-    print("Extracting data...")
-    
-def transform():
-    '''
-    Transform the data
-    '''
-    print("Transforming data...")
-    
+    print(f"Extracting data from Kaggle...")
+
+    # Load the latest version
+    path = kagglehub.dataset_download("irkaal/foodcom-recipes-and-reviews")
+
+    recipes_df = pd.read_csv(f'{path}/recipes.csv')
+    reviews_df = pd.read_csv(f'{path}/reviews.csv')
+
+    print("First 5 records:", recipes_df.head())
+
+    return recipes_df, reviews_df
