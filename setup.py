74f42a318e0f188ad9242ac6c81edaef8f4f2cb0
Haran Nallasivan <haranku16@users.noreply.github.com>
2025-04-11 15:21:27 -0400

    Extracting dataset from Kaggle

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from scripts.etl.etl import extract
-from scripts.etl.etl import transform
 
 def main():
     '''
     Entry point for the setup script.
     '''
     extract()
-    transform()
 
 if __name__ == "__main__":
     main()
